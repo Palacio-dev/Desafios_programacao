fd389e3144e3bb210b5b87ec672c0a6df3e3ee3d
Palacio <lucaspalacioa@gmail.com>
2025-08-12 16:09:51 -0300

    Bugs no problema E corrigidos

--- Contest01/E.cpp
+++ Contest01/E.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int t; 
     cin >> t;
-    while (t != 0){
+    while (t > 0){
         int n;
         string cartas;
         cin >> n >> cartas;
@@ -13,6 +13,16 @@ int main(){
         bool aliceTemN = false;
         bool bobTem1 = false;
         bool bobTemN = false;
+        int quant_alice = 0;
+        int quant_bob = 0;
+        for (int i = 0; i < cartas.size(); i++){
+            if (cartas[i] == 'A'){
+                quant_alice += 1;
+            }
+            else{
+                quant_bob += 1;
+            }
+        }
         if (cartas[0] == 'A'){
             aliceTem1 = true;
         }
@@ -25,20 +35,28 @@ int main(){
         else{
             bobTemN = true;
         }
-
-        
-        if (aliceTem1 and bobTemN) {
-            cout << "Alice\n";
+        if (aliceTem1 and aliceTemN){
+            cout << "Alice" << endl;
         }
-        else if (bobTem1 and aliceTemN) {
-            cout << "Bob\n";
+        else if (bobTem1 and bobTemN){
+            cout << "Bob" << endl;
         }
-        else if (aliceTemN) {
-            cout << "Alice\n";
+        else if (aliceTem1 and bobTemN){
+            if (quant_bob == 1){
+                cout << "Alice" << endl;
+            }
+            else{
+                cout << "Bob" << endl;
+            }
         }
-        else {
-            cout << "Bob\n";
+        else if ((aliceTemN and bobTem1) or (bobTem1 and aliceTemN)){
+            if (cartas[size(cartas) - 2] == 'A'){
+                cout << "Alice" << endl;
+            }
+            else{
+                cout << "Bob" << endl;
+            }
         }
-        t--;
+    t--;    
     }
 }
