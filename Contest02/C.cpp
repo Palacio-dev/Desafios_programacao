59dec3a4a151b690393aa57a4879abc6aed418c5
Palaciolol <lucaspalacioa@gmail.com>
2025-08-22 16:25:08 -0300

    Corrigindo bugs de problemas da semana2

--- Contest02/C.cpp
+++ Contest02/C.cpp
@@ -1,53 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool verifica_valido(vector<char>& exp, int comeco){
-    int count = 0;
-    for (int i = comeco; i < exp.size(); i++){
-        if (count < 0){
-            return false;
-        }
-        if (exp[i] == '('){
-            count += 1;
-            continue;
-        }
-        else if (exp[i] == ')'){
-            count -= 1;
-        }
-    }
-    if (count == 0){
-        return true;
-    }
-    return false;
-}
-
-void read_string(vector<char>& exp, int n){
-    for (int i = 0; i < n; i++){
-        char temp;
-        cin >> temp;
-        exp.push_back(temp);
-    }
-}
-
 int main(){
     int n;
     cin >> n;
-    vector<char> exp;
-    bool continua = true;
-    int cont = 1;
-    read_string(exp, n);
-    if (!verifica_valido(exp, 0)){
-        while (continua){
-            exp.push_back(exp[cont-1]);
-            if (!verifica_valido(exp, cont)){
-                cont++;
-                continue;
-            }
-            cout << cont << endl;
-            continua = false;
+    string s;
+    cin >> s;
+    
+    int count = 0;
+    int pos_crit = INT_MAX;
+    int pos = -1;
+
+    for (int i = 0; i < n;i++){
+        if (s[i] == '('){
+            count++;
+        }
+        else{
+            count--;
+        }            
+        if (count < pos_crit){
+            pos_crit = count;
+            pos = i;
         }
     }
-    else{
-        cout << 0 << endl;
-    }
+    cout << (pos+1)%n << endl;
 }
--- Contest02/D.cpp
+++ Contest02/D.cpp
@@ -1,66 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int soma_extremos(vector<long long>& nums, int pos){
-    int receita = nums[pos] + nums[nums.size() - pos-1];
-    return receita;
-}
-
-bool ta_no_intervalo(int soma, int min, int max){
-    if (soma <= max  and soma >= min){
-        return true;
-    }
-    return false;
+inline bool ta_no_intervalo(long long soma, long long lo, long long hi){
+    return (soma >= lo && soma <= hi);
 }
 
 int main(){
-    long long n, l;
-    vector<long long> nums;
-    vector<long long> ordenado;
-    cin >> n >> l;
-    long long menosL = -1 * l;
-    for (int i = 0; i < n; i++){
-        int temp;
-        cin >> temp;
-        nums.push_back(temp);
-    }
-    sort(nums.begin(), nums.end());
-    long long soma_temp;
+    long long n, L;
+    cin >> n >> L;
+    vector<long long> nums(n);
+    for (auto &x : nums) cin >> x;
+    long long lo = -L, hi = L - 1;
+
+    sort(nums.begin(), nums.end());               
+    vector<long long> ordenado; ordenado.reserve(n);
+
     long long receita = 0;
-    bool da_pra_somar = true;
-    soma_temp = soma_extremos(nums, 0);
-    if (!ta_no_intervalo(soma_temp, menosL, l-1)){
-        cout << "N" << endl;
-    }
-    else{
-        ordenado.push_back(nums[0]);
-        ordenado.push_back(nums[nums.size() -1]);
-        receita += soma_temp;
-        for (int i = 1; i < n-1; i++){
-            int temp1 = receita + nums[i];
-            int temp2 = receita + nums[nums.size() - i ];
-            if (ta_no_intervalo(temp1, menosL, l -1)){
-                receita += nums[i];
-                ordenado.push_back(nums[i]);
-                continue;
+    int l = 0, r = (int)n - 1;
+
+    auto cabe = [&](long long x){ return ta_no_intervalo(receita + x, lo, hi); };
+
+    while (l <= r) {
+        bool moveu = false;
+        if (receita >= 0) {
+            if (cabe(nums[l])){ 
+                receita += nums[l]; 
+                ordenado.push_back(nums[l]); 
+                l++; 
+                moveu = true; 
             }
-            else if (ta_no_intervalo(temp2, menosL, l -1)){
-                receita += nums[nums.size() - i ];
-                ordenado.push_back(nums[nums.size()-i-1]);
-                continue;
+            else if (cabe(nums[r])){ 
+                receita += nums[r]; 
+                ordenado.push_back(nums[r]); 
+                r--; 
+                moveu = true;
             }
-            da_pra_somar = false;
-            break;
-        }
-    
-        if (da_pra_somar){
-            cout << "S" << endl;
-            for (int i = 0; i < n;i++){
-                cout << ordenado[i] << " ";
-            }
-        }
+        } 
         else{
-            cout << "N" << endl;
+            if (cabe(nums[r])){ 
+                receita += nums[r];
+                ordenado.push_back(nums[r]); 
+                r--; 
+                moveu = true; 
+            }
+            else if (cabe(nums[l])){ 
+                receita += nums[l]; 
+                ordenado.push_back(nums[l]); 
+                l++; 
+                moveu = true; 
+            }
         }
+        if (!moveu){
+            cout << "N\n";
+            return 0;
+        }  
     }
+
+    cout << "S" << endl;;
+    for (int i = 0; i < n; i++){
+        if (i) cout << ' ';
+        cout << ordenado[i];
+    }
+    cout << endl;
+    return 0;
 }
